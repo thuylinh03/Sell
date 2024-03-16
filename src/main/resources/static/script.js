5961eee473e0503dc09ba691303c851ab9904814
thuylinh03 <21012508@st.phenikaa-uni.edu.vn>
2024-03-16 23:45:13 +0700

    add chatbot

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,6 @@
+document.getElementById("learn-more-btn").addEventListener("click", function() {
+    document.getElementById("banner").scrollIntoView({ behavior: "smooth" });
+});
+document.getElementById("shop-now-btnn").addEventListener("click", function() {
+    document.getElementById("products").scrollIntoView({ behavior: "smooth" });
+});
